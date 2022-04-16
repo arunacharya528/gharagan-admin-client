3e6159ea381744798727e01787a4720fc0bd61a7
Arun <arunacharya255@gmail.com>
2022-04-16 16:20:52 +0545

    Updated inventory edit form in product

--- src/pages/Product/InventoryEdit.js
+++ src/pages/Product/InventoryEdit.js
@@ -1,8 +1,30 @@
 import { Input, HelperText, Label, Select, Textarea } from '@windmill/react-ui'
-import React from 'react';
+import React, { useState } from 'react';
+import { CheckIcon } from '../../icons';
+import { Button } from '@windmill/react-ui'
 
-export const InventoryEdit = () => { 
+export const InventoryEdit = () => {
 
+    const [inventories, setinventories] = useState([]);
+
+    const [type, setType] = useState('');
+    const [price, setPrice] = useState(0);
+    const [discount, setDiscount] = useState(1);
+    const [status, setStatus] = useState(true);
+
+    const discountList = [
+        { id: 1, name: "Discount 1", discount_percentage: 5 },
+        { id: 2, name: "Discount 2", discount_percentage: 10 },
+        { id: 3, name: "Discount 3", discount_percentage: 15 }
+    ];
+
+    const getDiscountedPrice = (discountId) => {
+        return (price - (0.01 * discountList.find((element) => element.id === discount).discount_percentage * price))
+    }
+
+    const handleSubmission = () => {
+        console.log({ type, price, discount, status })
+    }
 
     return (
         <table className='table-auto w-full text-gray-600 dark:text-gray-400 '>
@@ -16,25 +38,31 @@ export const InventoryEdit = () => {
                 </tr>
                 <tr>
                     <th>
-                        <Input className="mt-1" placeholder="Jane Doe" />
+                        <Input className="mt-1" placeholder="Eg: Red, XL, L, etc" value={type} onChange={e => setType(e.target.value)} />
                     </th>
                     <th>
-                        <Input className="mt-1" placeholder="Jane Doe" />
+                        <Input className="mt-1" placeholder="Offering price" value={price} onChange={e => setPrice(e.target.value)} />
                     </th>
                     <th>
-                        <Input className="mt-1" placeholder="Jane Doe" />
-                    </th>
-                    <th>
-                        123
-                    </th>
-                    <th>
-                        <Select className="mt-1">
-                            <option value={'active'}>Active</option>
-                            <option value={'inactive'}>Inactive</option>
+                        <Select className="mt-1" value={discount} onChange={e => setDiscount(parseInt(e.target.value))}>
+                            {discountList.map((discount, index) =>
+                                <option value={discount.id} key={index}>{discount.name} - {discount.discount_percentage}%</option>
+                            )}
                         </Select>
                     </th>
                     <th>
-                        <button>Save</button>
+                        {getDiscountedPrice(discount)}
+                    </th>
+                    <th>
+                        <Select className="mt-1" value={status} onChange={e => setStatus(e.target.value)}>
+                            <option value={true}>Active</option>
+                            <option value={false}>Inactive</option>
+                        </Select>
+                    </th>
+                    <th>
+                        <div>
+                            <Button icon={CheckIcon} layout="link" aria-label="Like" onClick={handleSubmission} />
+                        </div>
                     </th>
                 </tr>
 
