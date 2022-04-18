1d00e53f744034f3830e2eafba68e89abe8704ba
Arun <arunacharya255@gmail.com>
2022-04-18 16:50:02 +0545

    Added spinner on loading

--- src/components/ThemedSuspense.js
+++ src/components/ThemedSuspense.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import spinner from "../assets/img/loading.gif"
 function ThemedSuspense() {
   return (
-    <div className="w-full h-screen p-6 text-lg font-medium text-gray-600 dark:text-gray-400 dark:bg-gray-900">
-      Loading...
+    <div className="w-full h-screen p-6 text-lg font-medium text-gray-600 dark:text-gray-400 dark:bg-gray-900 flex align-center">
+      <img src={spinner} className="w-32 h-32 m-auto" />
     </div>
   )
 }
