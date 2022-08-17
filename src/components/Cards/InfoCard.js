0dafbc57dab65901abae3987bd077467a941f30d
Arun <arunacharya255@gmail.com>
2022-08-17 17:22:08 +0545

    updated dashboard

--- src/components/Cards/InfoCard.js
+++ src/components/Cards/InfoCard.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Card, CardBody } from '@windmill/react-ui'
+import { Link } from 'react-router-dom'
 
-function InfoCard({ title, value, children: icon }) {
+function InfoCard({ title, value, children: icon, link }) {
   return (
     <Card>
       <CardBody className="flex items-center">
         {icon}
-        <div>
+        <div className='relative w-full'>
           <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">{title}</p>
           <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">{value}</p>
+          <div className="absolute bottom-0 right-0">
+            {link}
+          </div>
         </div>
       </CardBody>
     </Card>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,60 +1,59 @@
-import React, { useState, useEffect } from 'react'
-
-import CTA from '../components/CTA'
+import React from 'react'
 import InfoCard from '../components/Cards/InfoCard'
-import ChartCard from '../components/Chart/ChartCard'
-import { Doughnut, Line } from 'react-chartjs-2'
-import ChartLegend from '../components/Chart/ChartLegend'
 import PageTitle from '../components/Typography/PageTitle'
-import { ChatIcon, CartIcon, MoneyIcon, PeopleIcon } from '../icons'
+import { CartIcon, PeopleIcon, AdvertisementIcon } from '../icons'
 import RoundIcon from '../components/RoundIcon'
-import response from '../utils/demo/productData'
 import {
-  TableBody,
-  TableContainer,
-  Table,
-  TableHeader,
-  TableCell,
-  TableRow,
-  TableFooter,
-  Avatar,
-  Badge,
-  Pagination,
+  Button
 } from '@windmill/react-ui'
 
-import {
-  doughnutOptions,
-  lineOptions,
-  doughnutLegends,
-  lineLegends,
-} from '../utils/demo/chartsData'
 import Order from './Order/Order'
 import Rating from './Rating/Rating'
 import QuesitonAnswer from './QuestionAnswer/QuestionAnswer'
+import { useContext } from 'react'
+import { UserListContext } from '../context/UserListContext'
+import { ProductContext } from '../context/ProductContext'
+import { AdvertisementContext } from '../context/AdvertisementContext'
+import { Link } from 'react-router-dom'
 
 function Dashboard() {
-  const [page, setPage] = useState(1)
-  const [data, setData] = useState([])
 
-  // pagination setup
-  const resultsPerPage = 10
-  const totalResults = response.length
-
-  // pagination change control
-  function onPageChange(p) {
-    setPage(p)
-  }
-
-  // on page change, load new sliced data
-  // here you would make another server request for new data
-  useEffect(() => {
-    setData(response.slice((page - 1) * resultsPerPage, page * resultsPerPage))
-  }, [page])
+  const { users } = useContext(UserListContext)
+  const { products } = useContext(ProductContext)
+  const { advertisements } = useContext(AdvertisementContext)
 
   return (
     <>
       <PageTitle>Dashboard</PageTitle>
 
+      <div className="grid gap-6 mb-8 md:grid-cols-1 xl:grid-cols-3">
+        <InfoCard title="Total Users" value={users.length} link={<Button tag={Link} to="/app/user" layout="link">View more</Button>}>
+          <RoundIcon
+            icon={PeopleIcon}
+            iconColorClass="text-orange-500 dark:text-orange-100"
+            bgColorClass="bg-orange-100 dark:bg-orange-500"
+            className="mr-4"
+          />
+        </InfoCard>
+
+        <InfoCard title="Total Products" value={products.length} link={<Button tag={Link} to="/app/product" layout="link">View more</Button>}>
+          <RoundIcon
+            icon={CartIcon}
+            iconColorClass="text-blue-500 dark:text-blue-100"
+            bgColorClass="bg-blue-100 dark:bg-blue-500"
+            className="mr-4"
+          />
+        </InfoCard>
+
+        <InfoCard title="Total Advertisements" value={advertisements.length} link={<Button tag={Link} to="/app/advertisement" layout="link">View more</Button>}>
+          <RoundIcon
+            icon={AdvertisementIcon}
+            iconColorClass="text-teal-500 dark:text-teal-100"
+            bgColorClass="bg-teal-100 dark:bg-teal-500"
+            className="mr-4"
+          />
+        </InfoCard>
+      </div>
       <div className="grid grid-cols-2 gap-5">
         <div>
           <Order count={10} />
